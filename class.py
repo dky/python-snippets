10c21cce6a04936ca552e55f198b113e1555bc6f
Don Ky <don.d.ky@gmail.com>
2020-05-25 23:10:56 -0400

    Enhance class with vtype

--- class.py
+++ class.py
@@ -17,19 +17,37 @@ class Vehicle(object):
 
 class Vehicle:
 
-    def __init__(self, color, doors, tires):
+    def __init__(self, color, doors, tires, vtype):
         # constructor
         self.color = color
         self.doors = doors
         self.tires = tires
+        self.vtype = vtype
 
     # Methods, they are "methods once they live in a class"
     # Also notice every method has to have at least one argument self.
     # This is not true with a function.
     def brake(self):
         # stop the car
-        return "Braking"
+        return "%s braking" % self.vtype
 
     def drive(self):
         # drive the car
-        return "I'm driving"
+        return "I'm driving a %s %s!" % (self.color, self.vtype)
+
+# if __name__ == "__main__" means only run this code if it's executed as a
+# single standalone file. If you imported your module into another script
+# then the code underneath the conditional would not run.
+
+
+if __name__ == "__main__":
+    car = Vehicle("blue", 5, 4, "car")
+    print(car.color)
+    print(car.brake())
+    print(car.drive())
+
+    truck = Vehicle("red", 3, 6, "truck")
+    print(truck.color)
+    print(car.brake())
+    print(car.drive())
+
